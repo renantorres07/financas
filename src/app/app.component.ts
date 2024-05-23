05af04e7d69169c461083d2980e2ee6df3c040a3
Renan Torres <nantorres0@gmail.com>
2024-05-23 07:56:24 -0300

    Add test and karma files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   template: './app.component.html',
   styleUrls: ['./app.component.css']
 })
